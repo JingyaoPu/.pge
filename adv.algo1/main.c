43e3c185b4dd96fad3cf338fe4acdec59d39791c
Jingyao Pu <nicpu@nicdemacbook-pro.local>
2019-01-28 17:44:44 -0800

    Initial Commit

---
+++ adv.algo1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  adv.algo1
+//
+//  Created by Nic Pu on 1/28/19.
+//  Copyright © 2019 Nic Pu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
